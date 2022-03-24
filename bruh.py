058d61f73a4083e940758f384781cbf79c847354
danirapemethod <102331503+danirapemethod@users.noreply.github.com>
2022-03-24 18:45:27 -0400

    Add files via upload

---
+++ bruh.py
@@ -0,0 +1,49 @@
+name = input("name: ")
+niggaNames = ["NIGGERS", "BLACK", "GENDER", "KAGEPODS", "FUVO", "89UX"]
+
+
+def fatnigga72():
+    i = 1
+    while i < 72:
+        print("NIGGERS!?!?")
+        i += 1
+
+
+def danisimulator():
+    i = 1
+    while i < 72:
+        print("NIGGA SEND ME PICS FOR MY CP BAIT DAWG")
+        i += 1
+
+
+if name.upper() in niggaNames:
+    fatnigga72()
+else:
+    print("hi and the hey", name)
+
+age = input("How old are you: ")
+
+if float(age) < 18:
+    danisimulator()
+else:
+    print(age, "is not rapeable")
+
+
+race = input("What is your race: ")
+
+
+if race.upper() == "BLACK":
+    fatnigga72()
+else:
+    print("Good to see your normal ", name)
+
+
+shitrape52 = input("do you have any siblings under the age of 18 [Y]/[N]: ")
+
+
+if shitrape52.upper() == "Y":
+    fatnigga72()
+elif shitrape52.upper() == "N":
+    print("That is not ideal!")
+else:
+    print("nigga you retarded as hell put in a y or a n dumbass retard")
